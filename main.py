38866f12982c084ec3c4fd15351d527a0254e8d9
riffuu1 <pj85gyl@eduvaud.ch>
2026-04-27 11:32:41 +0200

    feat: main initialisation

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+from Player import Player
+
+pygame.init()
+
+#================
+# Window
+#================
+Width, Height = 400, 300
+screen = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("Chroniques du Kraken oublié")
+
+
+#==================
+# Background
+#==================
